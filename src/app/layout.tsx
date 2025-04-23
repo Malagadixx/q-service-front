0b7edfb1d87728191189371cd2e270b6e4bd6b13
Malagadixx <127902976+Malagadixx@users.noreply.github.com>
2025-04-23 18:37:06 -0300

    feat: criando a navbar, falta os icones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Q-Service",
   description: "Plataforma de autoatendimento para restaurantes",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="pt-BR" className={inter.className}>
-      <body>{children}</body>
+    <html lang="pt-BR" className="min-h-screen">
+      <body
+        className={`${inter.className} antialiased bg-white w-screen min-h-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import NavBar from "@/components/navbar";
+
 export default function Home() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl text-red-500">Hello World</h1>
-      </div>
+      <NavBar />
     </>
   );
 }
